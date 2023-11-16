26473d8260a30047977709123af0cf87d7d9cc84
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2023-11-16 14:41:07 -0600

    first line output correct

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -10,24 +10,32 @@ employee_todo = requests.get("https://jsonplaceholder.typicode.com/todos")
 # print(employee_user.status_code)
 # print(employee_user.json())
 
+input_id = int(sys.argv[1])
 
-name = employee_user.json()[0]['name']
+name = None
+for user in employee_user.json():
+	if user['id'] == input_id:
+		name = user['name']
+		break
 # print(name)
 
-completed = employee_todo.json()[0]['completed']
+# completed = employee_todo.json()[0]['completed']
 # print(completed)
 
 true_count = {}
 userid_count = {}
 
 for todo_entry in employee_todo.json():
-  userId = todo_entry['userId']
-  completed = todo_entry['completed']
+	userId = todo_entry['userId']
+	completed = todo_entry['completed']
 
-  userid_count[userId] = userid_count.get(userId, 0) + 1
+	if userId == input_id:
+		userid_count[userId] = userid_count.get(userId, 0) + 1
 
-  if completed:
-    true_count[userId] = true_count.get(userId, 0) + 1
+	if completed:
+		true_count[userId] = true_count.get(userId, 0) + 1
 
+completed_tasks = true_count.get(input_id, 0)
+total_tasks = userid_count.get(input_id, 0)
 
-print(f"Employee {name} is done with tasks ({true_count}/{userid_count}):")
+print(f"Employee {name} is done with tasks ({completed_tasks}/{total_tasks}):")
