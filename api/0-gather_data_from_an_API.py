5d40e565f24d694c762d3827364b12bd111a520e
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2023-11-29 10:12:30 -0600

    display title of completed tasks

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,9 +14,9 @@ input_id = int(sys.argv[1])
 
 name = None
 for user in employee_user.json():
-	if user['id'] == input_id:
-		name = user['name']
-		break
+    if user['id'] == input_id:
+        name = user['name']
+        break
 # print(name)
 
 # completed = employee_todo.json()[0]['completed']
@@ -26,14 +26,14 @@ true_count = {}
 userid_count = {}
 
 for todo_entry in employee_todo.json():
-	userId = todo_entry['userId']
-	completed = todo_entry['completed']
+    userId = todo_entry['userId']
+    completed = todo_entry['completed']
 
-	if userId == input_id:
-		userid_count[userId] = userid_count.get(userId, 0) + 1
+    if userId == input_id:
+        userid_count[userId] = userid_count.get(userId, 0) + 1
 
-	if completed:
-		true_count[userId] = true_count.get(userId, 0) + 1
+    if completed:
+        true_count[userId] = true_count.get(userId, 0) + 1
 
 completed_tasks = true_count.get(input_id, 0)
 total_tasks = userid_count.get(input_id, 0)
@@ -43,4 +43,6 @@ print(f"Employee {name} is done with tasks ({completed_tasks}/{total_tasks}):")
 
 
 for todo_entry in employee_todo.json():
-	title = 
+    if todo_entry['userId'] == input_id and todo_entry['completed']:
+        title = todo_entry['title']
+        print("\t " + title)
